9686579b8d0f41bb92fafbde39764f887dcfcfa8
nld <nld@gen.lan>
2021-12-29 13:52:48 +0100

    Basic framework (visual grid)

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+extern crate sdl2;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Point;
+use sdl2::rect::Rect;
+use std::time::Duration;
+mod visual;
+mod vars;
+
+fn main() {
+    //initialize storage for strenght of pheromons
+    let mut p_food = vec![vec![0; vars::SIZEY as usize]; vars::SIZEX as usize];
+    let mut p_home = vec![vec![0; vars::SIZEY as usize]; vars::SIZEX as usize];
+
+    //positions of stone
+    let mut stones = vec![vec![0; vars::SIZEY as usize]; vars::SIZEX as usize];
+
+    //just for testing
+    p_food[0][2] = 1;
+    p_food[0][0] = 1;
+    p_food[1][3] = 1;
+    p_food[1][3] = 1;
+    p_home[1][0] = 1;
+    p_home[3][3] = 1;
+    p_home[1][3] = 1;
+    printarr(&p_food);
+
+    //start visual
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("rust-sdl2 demo", vars::WINDOWRES[0] as u32, vars::WINDOWRES[1] as u32)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    //the main loop
+    'running: loop {
+        //backgroung color
+        canvas.set_draw_color(Color::RGB(27, 28, 30));
+        canvas.clear();
+
+        //looping through x, y
+        for x in 0..p_food.len(){
+            for y in 0..p_food[0].len(){
+                let food = p_food[x][y] != 0;
+                let home = p_home[x][y] != 0;
+                let tstone = stone[x][y] == 1;
+
+                if tstone {
+                    canvas.set_draw_color(Color::RGB(60, 56, 54));
+                    canvas.fill_rect(Rect::new(x as i32 * vars::BSX, y as i32 * vars::BSY, vars::BSX as u32, vars::BSY as u32));
+                } else {
+                    if food && !home{
+                        canvas.set_draw_color(Color::RGB(152, 151, 26));
+                        canvas.fill_rect(Rect::new(x as i32 * vars::BSX, y as i32 * vars::BSY, vars::BSX as u32, vars::BSY as u32));
+                    } else if !food && home {
+                        canvas.set_draw_color(Color::RGB(204, 36, 29));
+                        canvas.fill_rect(Rect::new(x as i32 * vars::BSX, y as i32 * vars::BSY, vars::BSX as u32, vars::BSY as u32));
+                    } else if food && home {
+                        canvas.set_draw_color(Color::RGB(250, 189, 47));
+                        canvas.fill_rect(Rect::new(x as i32 * vars::BSX, y as i32 * vars::BSY, vars::BSX as u32, vars::BSY as u32));
+                    }
+                }
+
+            }
+        }
+        canvas.present();
+
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..}  => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+fn printarr(arr: &Vec<Vec<i32>>) {
+    for x in 0..arr.len() {
+        let mut strx = String::from("");
+        for y in 0..arr[0].len() {
+            strx.push_str(&arr[x][y].to_string());
+        }
+        println!("{}", strx);
+    }
+}
---
+++ src/vars.rs
@@ -0,0 +1,10 @@
+pub const SIZEY :i32 = 4;
+pub const SIZEX :i32 = 6;
+pub const WINDOWRES :[i32; 2] = [1080, 720];
+pub const BSX :i32 = WINDOWRES[0] / SIZEX;
+pub const BSY :i32 = WINDOWRES[1] / SIZEY;
+/*
+pub const PHOME :i32 = 0;
+pub const PFOOD :i32 = 1;
+pub const RADIUS :i32 = 12;
+*/
